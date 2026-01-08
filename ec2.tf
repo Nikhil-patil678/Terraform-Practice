dfba1c50bb0c9c78e9ed4872eb84cb634c231c97
Nikhil Ishwar patil <np3250072@gmail.com>
2026-01-08 17:56:13 +0530

    Add Terraform configuration for multiple EC2 instances
    This Terraform configuration file defines multiple AWS EC2 instances with different configurations, including instances with static and dynamic tags.

---
+++ ec2.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+#First practical
+resource "aws_instance" "my-first-instance" {
+ami ="ami-068c0051b15cdb816"
+  instance_type = "t2.micro"
+  tags = {
+   Name = "web-server-1"
+}
+}
+
+resource "aws_instance" "my-second-instance" {
+ami ="ami-068c0051b15cdb816"
+  instance_type = "t2.micro"
+  tags = {
+   Name = "web-server-2"
+}
+}
+
+#Second Practical
+resource "aws_instance" "my-third-instance" {
+ami ="ami-068c0051b15cdb816"
+  instance_type = "t2.micro"
+  count = 3
+  tags = {
+   Name = "web-server${count.index}"
+}
+}
+
+# Third Practical
+resource "aws_instance" "my-third-instance" {
+ami ="ami-068c0051b15cdb816"
+  instance_type = "t2.micro"
+  count = 3
+  tags = {
+   Name = "web-server${count.index+1}"
+}
+}
+
+#Fourth practical
+resource "aws_instance" "my-third-instance" {
+  for_each = toset(["app-server","db-server","proxy-server"])
+ami ="ami-068c0051b15cdb816"
+  instance_type = "t2.micro"
+  tags = {
+   Name = each.key
+}
+}
